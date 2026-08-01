3a6252b549b011d9af8800af84a7e7af0cf8595d
W0of27 <ianwaje2027@gmail.com>
2026-08-01 15:13:57 +0800

    Edited the test values back to their proper values

--- src/main/java/com/w0of26/martialspells/spells/GuardiansCovenantSpell.java
+++ src/main/java/com/w0of26/martialspells/spells/GuardiansCovenantSpell.java
@@ -38,7 +38,7 @@ public final class GuardiansCovenantSpell extends AbstractSpell {
             10,
             12,
             14,
-            20 //16 is the actual value, this is for testing purposes only
+            16
     };
 
     private static final double[] RADIUS_BLOCKS = {
@@ -54,7 +54,7 @@ public final class GuardiansCovenantSpell extends AbstractSpell {
             100,
             80,
             60,
-            20 //40 is the actual cooldown, this is for testing purposes
+            40
     };
 
     private static final float[] REDIRECT_PERCENTAGES = {
