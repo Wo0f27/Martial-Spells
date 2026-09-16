8cb3cacb3d3ee55d5c1c477c644a0e08451f0fda
W0of26 <141473476+Wo0f27@users.noreply.github.com>
2026-09-16 10:32:05 +0800

    R7: add Bear Trap model animations

---
+++ src/main/java/com/w0of26/martialspells/client/model/BearTrapAnimations.java
@@ -0,0 +1,84 @@
+package com.w0of26.martialspells.client.model;
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+/** Mojmap translation of the frozen Rogues Bear Trap Blockbench clips. */
+public final class BearTrapAnimations {
+    private BearTrapAnimations() {
+    }
+
+    public static final AnimationDefinition SPAWN = AnimationDefinition.Builder.withLength(1.0F)
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.35F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.degreeVec(0, -10, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.6F, KeyframeAnimations.degreeVec(0, 5, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.7F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(1.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.POSITION,
+                    new Keyframe(0.0F, KeyframeAnimations.posVec(0, 14, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.05F, KeyframeAnimations.posVec(0, 16, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.15F, KeyframeAnimations.posVec(0, 14, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.posVec(0, -0.08F, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.5F, KeyframeAnimations.posVec(0, 1.93F, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.7F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(1.0F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.SCALE,
+                    new Keyframe(0.0F, KeyframeAnimations.scaleVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.3F, KeyframeAnimations.scaleVec(1, 1, 1), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.45F, KeyframeAnimations.scaleVec(1, 1, 1), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("beartrap_part_1", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(-67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.degreeVec(-67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.5F, KeyframeAnimations.degreeVec(4.9715F, 0.0231F, -0.5317F), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.65F, KeyframeAnimations.degreeVec(-19.921F, -0.2999F, -1.7082F), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.75F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.9F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("beartrap_part_2", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.degreeVec(67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.5F, KeyframeAnimations.degreeVec(-9.9426F, 0.0922F, 1.0594F), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.65F, KeyframeAnimations.degreeVec(17.4314F, -0.2301F, 1.5017F), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.75F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.9F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .build();
+
+    public static final AnimationDefinition IDLE = AnimationDefinition.Builder.withLength(0.2F)
+            .looping()
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.2F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .build();
+
+    public static final AnimationDefinition ATTACK = AnimationDefinition.Builder.withLength(1.5F)
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.POSITION,
+                    new Keyframe(0.0F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.05F, KeyframeAnimations.posVec(0, 0.25F, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.1F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(1.0F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(1.5F, KeyframeAnimations.posVec(0, -9, 0), AnimationChannel.Interpolations.CATMULLROM)))
+            .addAnimation("beartrap_part_1", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.05F, KeyframeAnimations.degreeVec(-67.5F, 0, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.1F, KeyframeAnimations.degreeVec(-67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("beartrap_part_2", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.05F, KeyframeAnimations.degreeVec(67.5F, 0, 0), AnimationChannel.Interpolations.CATMULLROM),
+                    new Keyframe(0.1F, KeyframeAnimations.degreeVec(67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .build();
+
+    public static final AnimationDefinition DESPAWN = AnimationDefinition.Builder.withLength(1.0F)
+            .addAnimation("root", new AnimationChannel(AnimationChannel.Targets.POSITION,
+                    new Keyframe(0.0F, KeyframeAnimations.posVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.75F, KeyframeAnimations.posVec(0, -11, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("beartrap_part_1", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.degreeVec(-67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .addAnimation("beartrap_part_2", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+                    new Keyframe(0.0F, KeyframeAnimations.degreeVec(0, 0, 0), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.45F, KeyframeAnimations.degreeVec(67.5F, 0, 0), AnimationChannel.Interpolations.LINEAR)))
+            .build();
+}
